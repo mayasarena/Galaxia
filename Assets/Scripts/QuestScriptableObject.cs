1cccc53bead8454965b6b6b9622c2f304e291c6b
Maya Murad <mmurad7@uwo.ca>
2021-03-24 13:47:27 -0400

    quest scriptable object

---
+++ Assets/Scripts/QuestScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu()]
+public class QuestScriptableObject : ScriptableObject 
+{
+    public Text questDescription;
+    public bool completed;
+    public GameObject itemButton;
+}
