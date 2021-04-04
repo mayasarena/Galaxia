46c86a3085327c55b3ecc2c574833b178af6f57e
Maya Murad <mmurad7@uwo.ca>
2021-04-04 17:23:12 -0400

    .

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public GameObject musicPlayer;
+    void Awake() {
+
+        musicPlayer = GameObject.Find("BGMusic");
+
+        if (musicPlayer == null)
+        {
+            musicPlayer = this.gameObject;
+            musicPlayer.name = "BGMusic";
+            DontDestroyOnLoad(musicPlayer);
+
+        }
+         
+        else
+        {
+            if (this.gameObject.name != "BGMusic")
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
