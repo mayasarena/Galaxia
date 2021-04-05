d74abe61530c8438300fbd113b6c2eb27295bb6a
Maya Murad <mmurad7@uwo.ca>
2021-04-05 19:26:43 -0400

    i think its finalized

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -8,13 +8,17 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 entrancePosition;
     public VectorValueScriptableObject entrancePositionVector;
+    public bool outside;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player" && !other.isTrigger)
         {
-            FindObjectOfType<GameManager>().GetComponent<SaveAndLoad>().Save();
-            FindObjectOfType<GameManager>().GetComponent<SaveSceneState>().SavePositions();
+            if (outside)
+            {
+                FindObjectOfType<GameManager>().GetComponent<SaveAndLoad>().Save();
+                FindObjectOfType<GameManager>().GetComponent<SaveSceneState>().SavePositions();
+            }
             entrancePositionVector.value = entrancePosition;
             SceneManager.LoadScene(sceneToLoad);
         }
